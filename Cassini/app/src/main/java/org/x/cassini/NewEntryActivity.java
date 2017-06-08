f0ebad96ffa7d14a58f5c46ea5c296e0fa536c54
Guo Mingxuan <carlguo26@gmail.com>
2017-06-08 09:32:47 +0800

    added button placeholders
    for weather, emotion, star and tag

--- Cassini/app/src/main/java/org/x/cassini/NewEntryActivity.java
+++ Cassini/app/src/main/java/org/x/cassini/NewEntryActivity.java
@@ -1,9 +1,12 @@
 package org.x.cassini;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Guo Mingxuan on 2017/6/7 0007.
@@ -13,6 +16,8 @@ public class NewEntryActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private String TAG = "NewEntry";
+    private TextView time, location;
+    private Button BWeather;
 
     @Override
     protected void onCreate(Bundle onSavedInstance) {
@@ -23,8 +28,8 @@ public class NewEntryActivity extends AppCompatActivity {
 
         // initialize various components
         initToolbar();
-
-
+        initTextView();
+        initFloatingActionButtons();
 
     }
 
@@ -38,5 +43,12 @@ public class NewEntryActivity extends AppCompatActivity {
         Log.d(TAG, "Title is " + getSupportActionBar().getTitle());
     }
 
+    private void initTextView() {
+        time = (TextView) findViewById(R.id.new_entry_time);
+        location = (TextView) findViewById(R.id.new_entry_location);
+    }
 
+    private void initFloatingActionButtons() {
+        BWeather = (Button) findViewById(R.id.new_entry_weather);
+    }
 }
