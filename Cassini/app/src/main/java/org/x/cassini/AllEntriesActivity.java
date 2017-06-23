ef4dba15fabc56d6cb231cdb00a9146342e8a759
Guo Mingxuan <carlguo26@gmail.com>
2017-06-23 16:20:58 +0800

    Added display of all entries

--- Cassini/app/src/main/java/org/x/cassini/AllEntriesActivity.java
+++ Cassini/app/src/main/java/org/x/cassini/AllEntriesActivity.java
@@ -1,16 +1,109 @@
 package org.x.cassini;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
- * Created by Administrator on 2017/6/15 0015.
+ * Created by Guo Mingxuan on 2017/6/15 0015.
  */
 
 public class AllEntriesActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private Context mContext;
+    private String TAG = "AllEntries";
+    private ArrayList<Storie> stories;
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle onSavedInstance) {
+        super.onCreate(onSavedInstance);
+        setContentView(R.layout.activity_all_entries);
+        mContext = getApplication();
 
+        formStoriesArray();
+        initToolbar();
+
+        initList();
+
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.all_entries_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("All Entries");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        Log.d(TAG, "Title is " + getSupportActionBar().getTitle());
+    }
+
+    private void formStoriesArray() {
+        File sdCard = Environment.getExternalStorageDirectory();
+        File dir = new File (sdCard.getAbsolutePath() + "/Cassini/");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        File[] files = dir.listFiles();
+        if (files == null) {
+            Toast.makeText(mContext, "No record found!", Toast.LENGTH_SHORT).show();
+        } else {
+            stories = new ArrayList<>();
+            for (File file : files) {
+                String dateTime, mainText, location;
+                ArrayList<String> tagList = new ArrayList<>();
+                int count;
+                try {
+                    FileInputStream fis = new FileInputStream(file);
+                    BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+                    // read date and time
+                    dateTime = br.readLine();
+                    Log.d(TAG, "formStoriesArray: " + dateTime);
+                    // read location;
+                    location = br.readLine();
+                    Log.d(TAG, "formStoriesArray: " + location);
+                    // skip weather to star
+                    for (int i = 0; i < 4; i++) {
+                        br.readLine();
+                    }
+                    // read tags
+                    count = Integer.valueOf(br.readLine());
+                    Log.d(TAG, "formStoriesArray: " + count);
+                    for (int j = 0; j < count; j++) {
+                        tagList.add(br.readLine());
+                        Log.d(TAG, "formStoriesArray: " + tagList.get(j));
+                    }
+                    // read main text
+                    count = Integer.valueOf(br.readLine());
+                    char[] textChar = new char[count];
+                    br.read(textChar, 0, count);
+                    mainText = new String(textChar);
+                    Log.d(TAG, "formStoriesArray: " + mainText);
+                    Storie temp = new Storie(mainText, location, dateTime, tagList);
+                    stories.add(temp);
+                    Log.d(TAG, "formStoriesArray: storie added");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                // TODO close streams
+            }
+        }
+    }
+
+    private void initList() {
+        list = (ListView) findViewById(R.id.all_entries_list);
+        list.setAdapter(new AllEntriesListAdapter(mContext, stories));
     }
 }
---
+++ Cassini/app/src/main/java/org/x/cassini/AllEntriesListAdapter.java
@@ -0,0 +1,75 @@
+package org.x.cassini;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Guo Mingxuan on 2017/6/23 0023.
+ */
+
+class AllEntriesListAdapter extends BaseAdapter {
+    private Context mContext;
+    private ArrayList<Storie> stories;
+    private String TAG = "AEListAdapter";
+
+    public AllEntriesListAdapter(Context context, ArrayList<Storie> stories) {
+        mContext = context;
+        this.stories = stories;
+    }
+
+    @Override
+    public int getCount() {
+        return stories.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view;
+        TextView main, location, date;
+        ArrayList<String> tagList;
+        Storie storie;
+        if (convertView == null) {
+            storie = stories.get(position);
+            tagList = storie.getmTagList();
+            view = inflater.inflate(R.layout.all_entries_rows, null);
+            // setup textviews
+            main = (TextView) view.findViewById(R.id.all_entries_list_main_text);
+            main.setText(storie.getmMainText());
+            location = (TextView) view.findViewById(R.id.all_entries_list_location);
+            location.setText(storie.getmLocation());
+            date = (TextView) view.findViewById(R.id.all_entries_list_time);
+            date.setText(storie.getmDateTime());
+            // add tags programmatically to the right of location
+            LinearLayout tags = (LinearLayout) view.findViewById(R.id.all_entries_list_tags);
+            for (String tag : tagList) {
+                TextView tagView = new TextView(mContext);
+                tagView.setTextColor(view.getResources().getColor(R.color.black));
+                tagView.setText(tag);
+                tags.addView(tagView);
+            }
+        } else {
+            view = convertView;
+            Log.e(TAG, "getView: convertView is not null");
+        }
+        return view;
+    }
+}
