da7014ef63bf859ca33a12c81a421643c25033cf
Shuang Yang <david.yangshuang@icloud.com>
2017-07-09 23:32:57 +0800

    added tagsview activity (part of)

---
+++ Cassini/app/src/main/java/org/x/cassini/TagsviewActivity.java
@@ -0,0 +1,13 @@
+package org.x.cassini;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TagviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tagview);
+    }
+}
---
+++ Cassini/app/src/main/java/org/x/cassini/TagsviewAdapter.java
@@ -0,0 +1,8 @@
+package org.x.cassini;
+
+/**
+ * Created by shuangyang on 9/7/17.
+ */
+
+public class TagsviewAdapter {
+}
