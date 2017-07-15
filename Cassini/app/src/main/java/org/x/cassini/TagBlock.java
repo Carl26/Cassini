4d1c7d642c5a0cdcaceb0fbe2cb8e619a21f1a66
Shuang Yang <david.yangshuang@icloud.com>
2017-07-15 14:34:02 +0800

    edited tagblocks and adapter

---
+++ Cassini/app/src/main/java/org/x/cassini/TagBlock.java
@@ -0,0 +1,30 @@
+package org.x.cassini;
+
+import java.util.ArrayList;
+
+/**
+ * Created by shuangyang on 13/7/17.
+ */
+
+public class TagBlock {
+    private ArrayList<String> mTagNames;
+    private char mAlphabet;
+    public TagBlock(char alphabet, ArrayList<String> tagNames) {
+        mAlphabet = alphabet;
+        mTagNames = tagNames;
+    }
+
+    public char getmAlphabet() { return mAlphabet;}
+
+    public void setmAlphabet(char alphabet) { this.mAlphabet = alphabet;}
+
+    public ArrayList<String> getmTagNames() { return mTagNames;}
+
+    public void setmTagNames(ArrayList<String> tagNames) {this.mTagNames = tagNames;}
+
+    public int getTagCount() {return this.mTagNames.size();}
+
+    public boolean isEmpty(Object o) {
+        return (o instanceof TagBlock) && (getTagCount() == 0);
+    }
+}
--- Cassini/app/src/main/java/org/x/cassini/TagsviewAdapter.java
+++ Cassini/app/src/main/java/org/x/cassini/TagsviewAdapter.java
@@ -1,10 +1,61 @@
 package org.x.cassini;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
- * Created by shuangyang on 9/7/17.
+ * Created by shuangyang on 11/7/17.
  */
 
-public class TagsviewAdapter extends RecyclerView.Adapter<TagsviewAdapter.ViewHolder> {
+class TagsViewAdapter extends RecyclerView.Adapter<TagsViewAdapter.ViewHolder> {
+    private Context mContext;
+    private ArrayList<TagBlock> mTagBlocks;
+    private String TAG = "TagsViewAdapter";
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tagAlphabet;
+        View divider;
+        RecyclerView tagList;
+
+        ViewHolder(View view) {
+            super(view);
+            tagAlphabet = (TextView) view.findViewById(R.id.tag_alphabet);
+            divider = (View) view.findViewById(R.id.tag_divider);
+            tagList = (RecyclerView) view.findViewById(R.id.tag_list);
+        }
+
+    }
+
+    public TagsViewAdapter(ArrayList<TagBlock> tagBlocks) {
+        mTagBlocks = tagBlocks;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.tag_block, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        TagBlock tagBlock = mTagBlocks.get(position);
+        holder.tagAlphabet.setText(tagBlock.getmAlphabet());
+        TagBlockAdapter adapter = new TagBlockAdapter(tagBlock.getmTagNames());
+        holder.tagList.setAdapter(adapter);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTagBlocks.size();
+    }
+
 }
