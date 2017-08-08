32377f3220901232f48fc12a31de047e40d0ee48
Guo Mingxuan <carlguo26@gmail.com>
2017-08-08 18:04:57 +0800

    Added delete support in tag entries activity
    1. fixed refreshing list after async db process
    2. implemented new adapter for activity casting

---
+++ Cassini/app/src/main/java/org/x/cassini/TagEntriesListAdapter.java
@@ -0,0 +1,117 @@
+package org.x.cassini;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Guo Mingxuan on 2017/6/23 0023.
+ */
+
+class TagEntriesListAdapter extends BaseAdapter {
+    private Context mContext;
+    private ArrayList<Storie> stories;
+    private String TAG = "TEListAdapter";
+    private boolean isMultiple = false;
+
+    public TagEntriesListAdapter(Context context, ArrayList<Storie> stories) {
+        mContext = context;
+        this.stories = stories;
+    }
+
+    class ViewHolder {
+        TextView main, location, date, month;
+        LinearLayout tags;
+        CheckBox checkBox;
+    }
+
+    @Override
+    public int getCount() {
+        return stories.size();
+    }
+
+    @Override
+    public Storie getItem(int position) {
+        return stories.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public void setIsMultiple(boolean isMultiple) {
+        this.isMultiple = isMultiple;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view;
+        ViewHolder holder;
+        Storie storie;
+        ArrayList<String> tagList;
+        ArrayList<Boolean> selectedItems = ((TagEntriesActivity) mContext).getSelectedItems();
+        if (convertView == null) {
+            holder = new ViewHolder();
+            view = inflater.inflate(R.layout.all_entries_rows, null);
+            // setup textviews
+            holder.main = (TextView) view.findViewById(R.id.all_entries_list_main_text);
+            holder.location = (TextView) view.findViewById(R.id.all_entries_list_location);
+            holder.date = (TextView) view.findViewById(R.id.all_entries_list_time_date);
+            holder.month = (TextView) view.findViewById(R.id.all_entries_list_time_month);
+            holder.tags = (LinearLayout) view.findViewById(R.id.all_entries_list_tags);
+            holder.checkBox = (CheckBox) view.findViewById(R.id.all_entries_checkbox);
+            view.setTag(holder);
+        } else {
+            view = convertView;
+            holder = (ViewHolder) view.getTag();
+            holder.main.setText("");
+            holder.location.setText("");
+            holder.date.setText("");
+            holder.month.setText("");
+            holder.tags.removeAllViews();
+        }
+        storie = stories.get(position);
+        tagList = storie.getmTagList();
+        holder.main.setText(storie.getmMainText());
+        holder.location.setText(storie.getmLocation());
+        holder.date.setText(storie.getmDay());
+        holder.month.setText(storie.getmMonth());
+        // add tags programmatically to the right of location
+        if (tagList != null) {
+            for (String tag : tagList) {
+                View v = inflater.inflate(R.layout.all_entries_row_tag,null);
+                TextView tagView = (TextView) v.findViewById(R.id.tag_item);
+                tagView.setText(tag);
+                ((ViewGroup)tagView.getParent()).removeView(tagView);
+                holder.tags.addView(tagView);
+            }
+        }
+        if (isMultiple) {
+            holder.checkBox.setVisibility(View.VISIBLE);
+        } else {
+            holder.checkBox.setVisibility(View.GONE);
+        }
+        if (selectedItems != null) {
+            holder.checkBox.setChecked(selectedItems.get(position));
+        }
+        return view;
+    }
+}
